0f40a30f7d5f00b0001c4fa87d2ab4f73763d01b
joaov <joaovitorpereira07@gmail.com>
2025-11-02 14:32:29 -0300

    Estrutura de módulos spring: Core, Common e authmodule adicionados

--- main/authmodule/src/main/java/com/main/service/RoleService.java
+++ main/authmodule/src/main/java/com/main/service/RoleService.java
@@ -1,13 +1,22 @@
 package com.main.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.main.mapper.RoleMapper;
 import com.main.repository.RoleRepository;
-import org.springframework.stereotype.Service;
 
 @Service
 public class RoleService
 {
     private final RoleMapper roleMapper;
     private final RoleRepository roleRepository;
+    private final RoleCacheService roleCacheService;
+    @Autowired
+    public RoleService(RoleMapper roleMapper, RoleRepository roleRepository, RoleCacheService roleCacheService) {
+        this.roleMapper = roleMapper;
+        this.roleRepository = roleRepository;
+        this.roleCacheService = roleCacheService;
+    }
 
 }
